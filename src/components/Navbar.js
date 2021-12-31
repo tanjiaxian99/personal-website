dcc93ba20f200b6bcf3fa56b247bedb440794953
Tan Jia Xian <e0543981@u.nus.edu>
2021-12-31 23:11:15 +0800

    Add sticky navbar without links and animation

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="navbar-name">Jia Xian.</div>
+        <ul>
+          <li>home.</li>
+          <li>about.</li>
+          <li>experiences.</li>
+          <li>projects.</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
