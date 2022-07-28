62ac15fa418b170ee2388d895c5f3c31884b082d
Tan Jia Xian <e0543981@u.nus.edu>
2022-07-28 22:44:23 +0800

    Fix NavbarLink offset

--- src/components/NavbarLink/NavbarLink.js
+++ src/components/NavbarLink/NavbarLink.js
@@ -4,7 +4,7 @@ import useBreakpointValue from "../../hooks/useBreakpointValue";
 import styles from "./NavbarLink.module.scss";
 
 const NavbarLink = ({ link }) => {
-  const offset = useBreakpointValue({ base: -60, desktop: -40 });
+  const offset = useBreakpointValue({ base: -50, desktop: -30 });
 
   return (
     <Link
@@ -13,7 +13,7 @@ const NavbarLink = ({ link }) => {
       spy={true}
       smooth={true}
       duration={1000}
-      offset={-40}
+      offset={offset}
       className={styles.link}
     >
       {link}.
